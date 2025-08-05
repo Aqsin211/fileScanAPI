ad134a45675d9b8f68852355ad384b3c177dd2fb
Aqsin Qurbanov <aqsincs@gmail.com>
2025-08-05 23:46:13 +0400

    first commit

---
+++ src/test/java/az/company/filescanner/FileScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.filescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
